51db1c5d09e8fd95fe71b0537f529e2707562d5d
Tushargupta9800 <tushargupta9800@gmail.com>
2021-09-24 23:36:34 +0530

    second

--- first.py
+++ first.py
@@ -4,14 +4,14 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-#@app.route('/api', methods=['GET'])
-#def hello_world():
-#    d = {}
-#    d['Query'] = str(request.args['Query'])
-#    return jsonify(d)
-#
-#if __name__ == '__main__':
-#    app.run()
+@app.route('/api', methods=['GET'])
+def hello_world():
+    d = {}
+    d['Query'] = str(request.args['Query'])
+    return jsonify(d)
+
+if __name__ == '__main__':
+    app.run()
 
 cluster = MongoClient("mongodb+srv://Algoristy:Algoristy@fakenewsdetector.gv1x5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster['test']
