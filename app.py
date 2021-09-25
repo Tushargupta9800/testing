d7c98c2a6c6e9cfb165a100393c1b711b45478c5
Tushargupta9800 <tushargupta9800@gmail.com>
2021-09-25 14:46:20 +0530

    ten

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def hello_world():
+    
+    cluster = MongoClient("mongodb+srv://Algoristy:Algoristy@fakenewsdetector.gv1x5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = cluster['test']
+    collection = db['test']
+    post = {"_id": str(request.args['Query']), "name": "tushar", "score":5}
+    collection.insert_one(post)
     d = {}
     d['Query'] = str(request.args['Query'])
     return jsonify(d)
