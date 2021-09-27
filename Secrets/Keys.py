9e1fb4ab91f4ac01ad7892941293f1e5c7f1ee7b
Tushargupta9800 <tushargupta9800@gmail.com>
2021-09-27 20:17:01 +0530

    13

---
+++ Secrets/Keys.py
@@ -0,0 +1,3 @@
+Username = "Algoristy"
+Password = "Algoristy"
+MongoClientId = "mongodb+srv://" + Username + ":" + Password + "@fakenewsdetector.gv1x5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import Secrets.Keys
 import pymongo
 from pymongo import MongoClient
 
@@ -6,15 +7,13 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def hello_world():
-    
-    cluster = MongoClient("mongodb+srv://Algoristy:Algoristy@fakenewsdetector.gv1x5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MongoClientId = Secrets.Keys.MongoClientId
+    cluster = MongoClient(MongoClientId)
     db = cluster['test']
     collection = db['test']
     post = {"_id": str(request.args['Query']), "name": "tushar", "score":5}
     collection.insert_one(post)
-    d = {}
-    d['Query'] = str(request.args['Query'])
-    return jsonify(d)
+    return post
 
 if __name__ == '__main__':
     app.run()
